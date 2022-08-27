577c86966e6317c6fbeea37ae62a0d2e9b8fdc1a
Pham Huu Hien <phhien203@gmail.com>
2022-08-27 22:39:33 +0700

    feat: Use async config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,16 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        type: 'postgres',
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+      }),
+    }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         DATABASE_HOST: Joi.required(),
@@ -17,16 +27,6 @@ import appConfig from './config/app.config';
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,5 +3,9 @@ export default () => ({
   database: {
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
+    type: 'postgres',
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
   },
 });
