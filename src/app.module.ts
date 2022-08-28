30ba7bbf50c1ae43f59efac7b32d4c279046f742
Pham Huu Hien <phhien203@gmail.com>
2022-08-28 12:23:07 +0700

    feat: Use config async

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,14 +10,11 @@ import appConfig from './config/app.config';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
-        type: 'postgres',
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-      }),
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return configService.get('database');
+      },
+      inject: [ConfigService],
     }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
